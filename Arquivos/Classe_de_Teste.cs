a01319d85aec453511bdc72ba8d848095ec313b0
Matheus Samasquini <114921819+Samasquini@users.noreply.github.com>
2024-07-30 08:52:40 -0400

    commit for project folders

---
+++ Arquivos/Classe_de_Teste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Educa_Sonho_Meu.Arquivos
+{
+    internal class Classe_de_Teste
+    {
+    }
+}
