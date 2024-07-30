9159c6a654db66f47776a2cd85707ad54be319de
Matheus Samasquini <114921819+Samasquini@users.noreply.github.com>
2024-07-30 08:41:45 -0400

    first commit on aplication

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Projeto_Educa_Sonho_Meu
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
