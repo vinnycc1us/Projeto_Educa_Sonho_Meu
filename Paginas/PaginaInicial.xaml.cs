c17f0bf2089bf48740adfe715248bd71f2e23ebe
Matheus Samasquini <114921819+Samasquini@users.noreply.github.com>
2024-08-06 09:56:54 -0300

    modifications
    Creation of the student registration screen

---
+++ Paginas/PaginaInicial.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Projeto_Educa_Sonho_Meu.Paginas
+{
+    public partial class PaginaInicial : Window
+    {
+        public PaginaInicial()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
