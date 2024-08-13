e257076afe2b83a40e06ee213557d7e1183e4a47
Carolina Alves <carolinaalvesdesouza98@gmail.com>
2024-08-13 09:19:54 -0400

    adicionamento da tela CadastroTurmar
    crie um page com o nome CadastroTurma e comecei a fazer o front, porem não está completo

---
+++ Paginas/CadastroTurma.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projeto_Educa_Sonho_Meu.Paginas
+{
+    /// <summary>
+    /// Interação lógica para CadastroTurma.xam
+    /// </summary>
+    public partial class CadastroTurma : Page
+    {
+        public CadastroTurma()
+        {
+            InitializeComponent();
+        }
+    }
+}
