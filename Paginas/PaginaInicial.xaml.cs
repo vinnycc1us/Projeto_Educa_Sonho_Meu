7efb2db8d6efbe88071fe7836a8a329837c5aa9a
Vinicius Inacio de Cristo <viniciusinacio687@gmail.com>
2024-09-15 18:22:40 -0400

    tela-inicial-TERMINEEEEEIIIIII-AMEEEEEMSENHOOOR

--- Paginas/PaginaInicial.xaml.cs
+++ Paginas/PaginaInicial.xaml.cs
@@ -28,7 +28,6 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             framepaginainicial.Navigate(new CadastrarAluno());
         }
 
-
         private void btnCadastrarAluno(object sender, RoutedEventArgs e)
         {
             // Instancia a nova janela
@@ -45,6 +44,22 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             framepaginainicial.Navigate(new ConsultarAluno());
         }
 
+        private void btnAluno(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarAluno.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarAluno.Visibility = Visibility.Visible;
+                nbtnConsultarAluno.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarAluno.Visibility = Visibility.Collapsed;
+                nbtnConsultarAluno.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
         private void btnCadastrarTurma(object sender, RoutedEventArgs e)
         {
             // Instancia a nova janela
@@ -59,6 +74,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             Window window = new Window();
             window.Content = new ConsultarTurma();
             framepaginainicial.Navigate(new ConsultarTurma());
+        }
+
+        private void btnTurma(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarTurma.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarTurma.Visibility = Visibility.Visible;
+                nbtnConsultarTurma.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarTurma.Visibility = Visibility.Collapsed;
+                nbtnConsultarTurma.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void btnCadastrarFuncionario(object sender, RoutedEventArgs e)
@@ -77,6 +107,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             framepaginainicial.Navigate(new ConsultarFuncionario());
         }
 
+        private void btnFuncionario(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarFuncionario.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarFuncionario.Visibility = Visibility.Visible;
+                nbtnConsultarFuncionario.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarFuncionario.Visibility = Visibility.Collapsed;
+                nbtnConsultarFuncionario.Visibility = Visibility.Collapsed;
+            }
+        }
+
         private void btnCadastrarSala(object sender, RoutedEventArgs e)
         {
             // Instancia a nova janela
@@ -91,6 +136,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             Window window = new Window();
             window.Content = new ConsultarSala();
             framepaginainicial.Navigate(new ConsultarSala());
+        }
+
+        private void btnSala(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarSala.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarSala.Visibility = Visibility.Visible;
+                nbtnConsultarSala.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarSala.Visibility = Visibility.Collapsed;
+                nbtnConsultarSala.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void btnCadastrarSonhoEntrada(object sender, RoutedEventArgs e)
@@ -109,6 +169,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             framepaginainicial.Navigate(new CadastroSonhoSaida());
         }
 
+        private void btnSonho(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarSonhoEntrada.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarSonhoEntrada.Visibility = Visibility.Visible;
+                nbtnCadastrarSonhoSaida.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarSonhoEntrada.Visibility = Visibility.Collapsed;
+                nbtnCadastrarSonhoSaida.Visibility = Visibility.Collapsed;
+            }
+        }
+
         private void btnCadastrarEstoquePedagogico(object sender, RoutedEventArgs e)
         {
             // Instancia a nova janela
@@ -123,6 +198,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             Window window = new Window();
             window.Content = new ConsultarEstoquePedagogico();
             framepaginainicial.Navigate(new ConsultarEstoquePedagogico());
+        }
+
+        private void btnEstoquePedagogico(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarEstoquePedagogico.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarEstoquePedagogico.Visibility = Visibility.Visible;
+                nbtnConsultarEstoquePedagogico.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarEstoquePedagogico.Visibility = Visibility.Collapsed;
+                nbtnConsultarEstoquePedagogico.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void btnCadastrarBiblioteca(object sender, RoutedEventArgs e)
@@ -141,6 +231,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             framepaginainicial.Navigate(new ConsultarBiblioteca());
         }
 
+        private void btnBiblioteca(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarBiblioteca.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarBiblioteca.Visibility = Visibility.Visible;
+                nbtnConsultarBiblioteca.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarBiblioteca.Visibility = Visibility.Collapsed;
+                nbtnConsultarBiblioteca.Visibility = Visibility.Collapsed;
+            }
+        }
+
         private void btnCadastrarInstrumento(object sender, RoutedEventArgs e)
         {
             // Instancia a nova janela
@@ -155,6 +260,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             Window window = new Window();
             window.Content = new ConsultarInstrumento();
             framepaginainicial.Navigate(new ConsultarInstrumento());
+        }
+
+        private void btnInstrumento(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarInstrumento.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarInstrumento.Visibility = Visibility.Visible;
+                nbtnConsultarInstrumento.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarInstrumento.Visibility = Visibility.Collapsed;
+                nbtnConsultarInstrumento.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void btnCadastrarProjeto(object sender, RoutedEventArgs e)
@@ -173,6 +293,22 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             framepaginainicial.Navigate(new ConsultarProjeto());
         }
 
+        private void btnProjeto(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarProjeto.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarProjeto.Visibility = Visibility.Visible;
+                nbtnConsultarProjeto.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarProjeto.Visibility = Visibility.Collapsed;
+                nbtnConsultarProjeto.Visibility = Visibility.Collapsed;
+            }
+        }
+
+
         private void btnCadastrarRecursoFinanceiro(object sender, RoutedEventArgs e)
         {
             // Instancia a nova janela
@@ -189,6 +325,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             framepaginainicial.Navigate(new ConsultarRecursoFinanceiro());
         }
 
+        private void btnRecursoFinanceiro(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarRecursoFinanceiro.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarRecursoFinanceiro.Visibility = Visibility.Visible;
+                nbtnConsultarRecursoFinanceiro.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarRecursoFinanceiro.Visibility = Visibility.Collapsed;
+                nbtnConsultarRecursoFinanceiro.Visibility = Visibility.Collapsed;
+            }
+        }
+
         private void btnCadastrarCalendarioAcademico(object sender, RoutedEventArgs e)
         {
             // Instancia a nova janela
@@ -203,6 +354,21 @@ namespace Projeto_Educa_Sonho_Meu.Paginas
             Window window = new Window();
             window.Content = new ConsultarCalendarioAcademico();
             framepaginainicial.Navigate(new ConsultarCalendarioAcademico());
+        }
+
+        private void btnCalendarioAcademico(object sender, RoutedEventArgs e)
+        {
+            // Alternar a visibilidade dos botões
+            if (nbtnCadastrarCalendarioAcademico.Visibility == Visibility.Collapsed)
+            {
+                nbtnCadastrarCalendarioAcademico.Visibility = Visibility.Visible;
+                nbtnConsultarCalendarioAcademico.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                nbtnCadastrarCalendarioAcademico.Visibility = Visibility.Collapsed;
+                nbtnConsultarCalendarioAcademico.Visibility = Visibility.Collapsed;
+            }
         }
     }
 
