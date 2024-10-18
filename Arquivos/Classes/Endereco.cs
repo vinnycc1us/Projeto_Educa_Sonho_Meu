c73bb82240adafdb3cd813155fb2cb17561a79d8
Matheus Samasquini <114921819+Samasquini@users.noreply.github.com>
2024-10-18 13:55:19 -0300

    criação das classes (Endereço e Responsável)

---
+++ Arquivos/Classes/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
+{
+    internal class Endereco
+    {
+        int id_end;
+        string uf_end;
+        string cidade_end;
+        string bairro_end;
+        string numero_end;
+        string cep_end;
+    }
+}
---
+++ Arquivos/Classes/Responsavel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
+{
+    public class Responsavel
+    {
+        public int id_resp { get; set; }
+        public string nome_completo_resp { get; set; }
+        public string cpf_resp {  get; set; }
+        public string rg_resp { get; set; }
+        public string orgao_expeditor_resp { get; set; }
+        public string telefone_resp { get; set; }
+        public string telefone_alternativo_resp { get; set; }
+        public string telefone_fixo_resp { get; set; }
+        string telefone_recado_resp { get; set; }
+
+        public Responsavel(string nome_completo_resp, string cpf_resp, string rg_resp, string orgao_expeditor_resp, string telefone_resp, string telefone_alternativo_resp, string telefone_fixo_resp, string telefone_recado_resp)
+        {
+            this.nome_completo_resp = nome_completo_resp;
+            this.cpf_resp = cpf_resp;
+            this.rg_resp = rg_resp;
+            this.orgao_expeditor_resp = orgao_expeditor_resp;
+            this.telefone_resp = telefone_resp;
+            this.telefone_alternativo_resp = telefone_alternativo_resp;
+            this.telefone_fixo_resp = telefone_fixo_resp;
+            this.telefone_recado_resp = telefone_recado_resp;
+        }
+    }
+}
