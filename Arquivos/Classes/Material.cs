b15989bd745787914acb834b6cf75b5807cebc6e
Vitor_Oficial_Tururu <jvitoralvernaz@outlook.com>
2024-10-18 15:18:27 -0400

    Classes Funcionario e Material

---
+++ Arquivos/Classes/Material.cs
@@ -0,0 +1,20 @@
+﻿using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
+{
+    public class Material
+    {
+        
+        public int id_mate {  get; set; }
+        public string nome_mate {  get; set; }
+        public int quantidade_mate {  get; set; }
+        public string descricao_mate {  get; set; }
+        public double valor_mate {  get; set; }
+        public int id_sal_fk {  get; set; }
+    }
+}
