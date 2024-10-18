4dabbf0688698d8da16616b2d3c20e8357a783bb
Carolina Alves <carolinaalvesdesouza98@gmail.com>
2024-10-18 14:43:58 -0400

    classes Recursos e Despesas

---
+++ Arquivos/Classes/Despesas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
+{
+    public class Despesas
+    {
+        public int id_desp {  get; set; }
+        public string doador_desp { get; set; }
+        public double valor_desp { get; set; }
+        public string destino_desp { get; set; }
+        public DateTime data_desp { get; set; }
+
+        public Despesas(int id_desp, string doador_desp, double valor_desp, string destino_desp, DateTime data_desp)
+        {
+            this.id_desp = id_desp;
+            this.doador_desp = doador_desp;
+            this.valor_desp = valor_desp;
+            this.destino_desp = destino_desp;
+            this.data_desp = data_desp;
+        }
+    }
+}
