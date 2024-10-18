37bc2b7eb57773fa5fe5d75428a733170fd9708d
Matheus Samasquini <114921819+Samasquini@users.noreply.github.com>
2024-10-18 14:43:54 -0300

    Create Turma.cs

---
+++ Arquivos/Classes/Turma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
+{
+    internal class Turma
+    {
+    }
+}
