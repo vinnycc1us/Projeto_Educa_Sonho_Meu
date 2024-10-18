1003dcb89a8e8d2e35cafa7f61d66f5014d32ba4
Matheus Samasquini <114921819+Samasquini@users.noreply.github.com>
2024-10-18 14:53:17 -0300

    Criação da classe (Sala) e atualização da classe (Turma)

---
+++ Arquivos/Classes/Sala.cs
@@ -0,0 +1,26 @@
+﻿using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
+{
+    internal class Sala
+    {
+        public int id_sal {  get; set; }
+        public string nome_sal { get; set; }
+        public string localizacao_sal { get; set; }
+        public string capacidade_sal {get; set; }
+        public int id_turm_fk {get; set; }
+
+        public Sala(string nome_sal, string localizacao_sal, string capacidade_sal, int id_turm_fk)
+        {
+            this.nome_sal = nome_sal;
+            this.localizacao_sal = localizacao_sal;
+            this.capacidade_sal = capacidade_sal;
+            this.id_turm_fk = id_turm_fk;
+        }
+    }
+}
--- Arquivos/Classes/Turma.cs
+++ Arquivos/Classes/Turma.cs
@@ -8,5 +8,18 @@ namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
 {
     internal class Turma
     {
+        public int id_turm { get; set; }
+        public string nome_turm { get; set; }
+        public string quantidade_turm { get; set; }
+        public string descricao_turm { get; set; }
+        public int ano_turm { get; set; }
+
+        public Turma(string nome_turm, string quantidade_turm, string descricao_turm, int ano_turm)
+        {
+            this.nome_turm = nome_turm;
+            this.quantidade_turm = quantidade_turm;
+            this.descricao_turm = descricao_turm;
+            this.ano_turm = ano_turm;
+        }
     }
 }
