5d58bd87481f65115e474c812bf026a348281a43
Matheus Samasquini <114921819+Samasquini@users.noreply.github.com>
2024-11-11 21:25:54 -0300

    Update EscolaDAO.cs

--- Arquivos/Classes/EscolaDAO.cs
+++ Arquivos/Classes/EscolaDAO.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,98 +10,107 @@ namespace Projeto_Educa_Sonho_Meu.Arquivos.Classes
 {
     public class EscolaDAO
     {
-        private string connectionString;
+        private static Conexao _conn = new Conexao();
 
-        public EscolaDAO(string connectionString)
+        public void Insert(Escola escola)
         {
-            this.connectionString = connectionString;
-        }
-
-        public void Inserir(Escola escola)
-        {
-            using (var connection = new MySqlConnection(connectionString))
+            try
             {
-                connection.Open();
-                var command = new MySqlCommand("INSERT INTO Escola (nome_esc, id_end_fk) VALUES (@nome, @idEnd)", connection);
-                command.Parameters.AddWithValue("@nome", escola.nome_esc);
-                command.Parameters.AddWithValue("@idEnd", escola.id_end_fk);
+                var comando = _conn.Query();
+                comando.CommandText = "INSERT INTO Escola (nome_esc, id_end_fk) VALUES (@nome, @idEnd)";
+                comando.Parameters.AddWithValue("@nome", escola.Nome);
+                comando.Parameters.AddWithValue("@idEnd", escola.id_end_fk);
 
-                command.ExecuteNonQuery();
-            }
-        }
+                var resultado = comando.ExecuteNonQuery();
 
-        public void Atualizar(Escola escola)
-        {
-            using (var connection = new MySqlConnection(connectionString))
-            {
-                connection.Open();
-                var command = new MySqlCommand("UPDATE Escola SET nome_esc = @nome, id_end_fk = @idEnd WHERE id_esc = @id", connection);
-                command.Parameters.AddWithValue("@id", escola.id_esc);
-                command.Parameters.AddWithValue("@nome", escola.nome_esc);
-                command.Parameters.AddWithValue("@idEnd", escola.id_end_fk);
-
-                command.ExecuteNonQuery();
-            }
-        }
-
-        public void Deletar(int id_esc)
-        {
-            using (var connection = new MySqlConnection(connectionString))
-            {
-                connection.Open();
-                var command = new MySqlCommand("DELETE FROM Escola WHERE id_esc = @id", connection);
-                command.Parameters.AddWithValue("@id", id_esc);
-
-                command.ExecuteNonQuery();
-            }
-        }
-
-        public Escola BuscarPorId(int id_esc)
-        {
-            using (var connection = new MySqlConnection(connectionString))
-            {
-                connection.Open();
-                var command = new MySqlCommand("SELECT * FROM Escola WHERE id_esc = @id", connection);
-                command.Parameters.AddWithValue("@id", id_esc);
-
-                using (var reader = command.ExecuteReader())
+                if (resultado == 0)
                 {
-                    if (reader.Read())
-                    {
-                        return new Escola
-                        {
-                            id_esc = reader.GetInt32("id_esc"),
-                            nome_esc = reader.GetString("nome_esc"),
-                            id_end_fk = reader.GetInt32("id_end_fk")
-                        };
-                    }
+                    throw new Exception("Ocorreram erros ao salvar as informações");
                 }
+
             }
-            return null;
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        public List<Escola> ListarTodas()
+        public void Update(Escola escola)
         {
-            var escolas = new List<Escola>();
-            using (var connection = new MySqlConnection(connectionString))
+            try
             {
-                connection.Open();
-                var command = new MySqlCommand("SELECT * FROM Escola", connection);
+                var comando = _conn.Query();
+                comando.CommandText = "UPDATE Escola SET nome_esc = @nome, id_end_fk = @idEnd WHERE id_esc = @id";
+                comando.Parameters.AddWithValue("@id", escola.Id);
+                comando.Parameters.AddWithValue("@nome", escola.Nome);
+                comando.Parameters.AddWithValue("@idEnd", escola.id_end_fk);
 
-                using (var reader = command.ExecuteReader())
+                var resultado = comando.ExecuteNonQuery();
+
+                if (resultado == 0)
                 {
-                    while (reader.Read())
-                    {
-                        escolas.Add(new Escola
-                        {
-                            id_esc = reader.GetInt32("id_esc"),
-                            nome_esc = reader.GetString("nome_esc"),
-                            id_end_fk = reader.GetInt32("id_end_fk")
-                        });
-                    }
+                    throw new Exception("Ocorreram erros ao salvar as informações");
                 }
+
             }
-            return escolas;
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(Escola escola)
+        {
+            try
+            {
+                var comando = _conn.Query();
+
+                comando.CommandText = "DELETE FROM Escola WHERE id_esc = @id";
+                comando.Parameters.AddWithValue("@id", escola.Id);
+
+                var resultado = comando.ExecuteNonQuery();
+
+                if (resultado == 0)
+                {
+                    throw new Exception("Ocorreram erros ao salvar as informações.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Escola> List()
+        {
+            try
+            {
+                var lista = new List<Escola>();
+                var comando = _conn.Query();
+
+                comando.CommandText = "SELECT * FROM Escola";
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var escola = new Escola();
+                    escola.Id = reader.GetInt32("id_esc");
+                    escola.Nome = DAOHelper.GetString(reader, "nome_esc");
+                    escola.id_end_fk = reader.GetInt32("id_end_fk");
+                    lista.Add(escola);
+                }
+
+                reader.Close();
+
+                return lista;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
